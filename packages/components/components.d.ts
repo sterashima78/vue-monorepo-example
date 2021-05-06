233ed4742d9067be7862ec584f230b6900a7c3c2
sterashima <sterashima78@gmail.com>
2021-05-06 16:17:46 +0900

    create components package

---
+++ packages/components/components.d.ts
@@ -0,0 +1,7 @@
+import Vue, { PluginFunction, VueConstructor } from 'vue';
+
+
+declare const Components: PluginFunction<any>;
+export default Components;
+
+export const ComponentsSample: VueConstructor<Vue>;
